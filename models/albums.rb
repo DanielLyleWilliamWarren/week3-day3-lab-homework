de3cfb59698e18f5869739a251f735eebbef3157
Magdalena <m.mandat@gmail.com>
2018-06-06 17:20:17 +0100

    MVP complete

--- models/albums.rb
+++ models/albums.rb
@@ -10,6 +10,33 @@ def initialize(options)
 end
 
 
+def save()
+  sql = 'INSERT INTO albums
+  (title, genre, artist_id)
+  VALUES
+  ($1, $2, $3)
+  RETURNING id'
+  values = [@title, @genre, @artist_id]
+  result = SqlRunner.run(sql, values)
+  @id = result[0]['id'].to_i
+end
 
+def self.all()
+  sql = "SELECT * FROM albums"
+  albums = SqlRunner.run(sql)
+  return albums.map {|album| Album.new(album)}
+end
+
+def artist()
+  sql = "SELECT * FROM artists WHERE id = $1"
+  values = [@artist_id]
+  artist = SqlRunner.run(sql, values)
+  return Artist.new(artist[0])
+end
+
+def self.delete_all
+  sql = 'DELETE FROM albums'
+  SqlRunner.run(sql)
+end
 
 end
